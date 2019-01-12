5b3af22fc0e4a8f22ea123f3d8679067a930ceaf
Terri Hewitt <hewittt@oregonstate.edu>
2019-01-11 16:52:57 -0800

    Added decryption functions

--- cpp/crypto.cpp
+++ cpp/crypto.cpp
@@ -8,90 +8,159 @@
 #include <openssl/hmac.h>	
 #include <openssl/des.h>
 
-void triple_des(unsigned char* input, unsigned char* k, unsigned char* iv);
-void cast5(unsigned char* input, unsigned char* k, unsigned char* iv);
-void aes_ctr(unsigned char* input, unsigned char* k, unsigned char* iv);
+void triple_des_encrypt(unsigned char*, unsigned char*, unsigned char*, unsigned char*);
+void triple_des_decrypt(unsigned char*, unsigned char*, unsigned char*, unsigned char*);
+void cast5_encrypt(unsigned char*, unsigned char*, unsigned char*, unsigned char*);
+void cast5_decrypt(unsigned char*, unsigned char*, unsigned char*, unsigned char*);
+void aes_ctr_encrypt(unsigned char*, unsigned char*, unsigned char*, unsigned char*);
+void aes_ctr_decrypt(unsigned char*, unsigned char*, unsigned char*, unsigned char*);
 void sha_1(unsigned char*);
 void sha_2(unsigned char*);
 void hmac_sha2(unsigned char*, unsigned char*);
 void hmac_blake2s(unsigned char*, unsigned char*);
 void print_output(unsigned char*);
-
+void spacer(void);
 
 int main() {
-//	const char mssg[] = "This is the message.\0";
-	unsigned char* mssg = (unsigned char *)"message";
+//	const char plaintext[] = "This is the message.\0";
+	unsigned char* plaintext = (unsigned char *)"message";
 //	unsigned char* key = "1\0";
 	unsigned char* key256 = (unsigned char *)"01234567890123456789012345678901";
 //	unsigned char* key256 = "key256\0";
 	unsigned char* iv128 = (unsigned char *)"0123456789012345";
 //	unsigned char* iv128 = "iv128\0";
+	unsigned char ciphertext[512];
+	unsigned char decryptedtext[512];
 
-	sha_1(mssg);
-	sha_2(mssg);
-	hmac_sha2(mssg, key256);
-	hmac_blake2s(mssg, key256);
+	sha_1(plaintext);
+	sha_2(plaintext);
+	hmac_sha2(plaintext, key256);
+	hmac_blake2s(plaintext, key256);
+	spacer();
 
-	aes_ctr(mssg, key256, iv128);
-	cast5(mssg, key256, iv128);
-	triple_des(mssg, key256, iv128);
+	aes_ctr_encrypt(plaintext, ciphertext, key256, iv128);
+	aes_ctr_decrypt(ciphertext, decryptedtext, key256, iv128);
+	spacer();
+	cast5_encrypt(plaintext, ciphertext, key256, iv128);
+	cast5_decrypt(ciphertext, decryptedtext, key256, iv128);
+	spacer();
+	triple_des_encrypt(plaintext, ciphertext, key256, iv128);
+	triple_des_decrypt(ciphertext, decryptedtext, key256, iv128);
 
 	return 0;
 }
 
+void spacer(void){
+ printf("\n-----------------------------------------------------\n");
+}
+
 /***************CIPHERS*****************/
-void triple_des(unsigned char* input, unsigned char* k, unsigned char* iv) {
+void triple_des_encrypt(unsigned char* plaintext, unsigned char* ciphertext, unsigned char* k, unsigned char* iv) {
 	int length;
-	unsigned char ciphertext[512];
 	EVP_CIPHER_CTX *ctx;
 
 	ctx = EVP_CIPHER_CTX_new();
 
 	EVP_EncryptInit_ex(ctx, EVP_des_ede3(), NULL, k, iv);
-	EVP_EncryptUpdate(ctx, ciphertext, &length, input, strlen((char *)input));
+	EVP_EncryptUpdate(ctx, ciphertext, &length, plaintext, strlen((char *)plaintext));
 	EVP_EncryptFinal_ex(ctx, ciphertext + length, &length);
 	EVP_CIPHER_CTX_free(ctx);
 
-	printf("3DES Plaintext:\n");
-	printf("%s\n\n", input);
+	printf("3DES Initial Plaintext:\n");
+	printf("%s\n\n", plaintext);
 	printf("3DES Ciphertext (hex representation):\n");
-//	print_output(ciphertext);
+	print_output(ciphertext);
 }
 
-void cast5(unsigned char* input, unsigned char* k, unsigned char* iv) {
+void triple_des_decrypt(unsigned char* ciphertext, unsigned char* plaintext, unsigned char* k, unsigned char* iv) {
 	int length;
-	unsigned char ciphertext[512];
+	int plaintext_len = 0;
+	EVP_CIPHER_CTX *ctx;
+
+	ctx = EVP_CIPHER_CTX_new();
+
+	EVP_DecryptInit_ex(ctx, EVP_des_ede3(), NULL, k, iv);
+	EVP_DecryptUpdate(ctx, plaintext, &length, ciphertext, strlen((char *)ciphertext));
+	plaintext_len += length;
+	EVP_DecryptFinal_ex(ctx, plaintext + length, &length);
+	plaintext_len += length;
+	EVP_CIPHER_CTX_free(ctx);
+
+	printf("3DES Decrypted text:\n");
+	plaintext[plaintext_len] = '\0';
+	printf("%s\n\n", plaintext);
+}
+
+void cast5_encrypt(unsigned char* plaintext, unsigned char* ciphertext, unsigned char* k, unsigned char* iv) {
+	int length;
 	EVP_CIPHER_CTX *ctx;
 
 	ctx = EVP_CIPHER_CTX_new();
 
 	EVP_EncryptInit_ex(ctx, EVP_cast5_cbc(), NULL, k, iv);
-	EVP_EncryptUpdate(ctx, ciphertext, &length, input, strlen((char *)input));
+	EVP_EncryptUpdate(ctx, ciphertext, &length, plaintext, strlen((char *)plaintext));
 	EVP_EncryptFinal_ex(ctx, ciphertext + length, &length);
 	EVP_CIPHER_CTX_free(ctx);
 
-	printf("CAST5 Plaintext:\n");
-	printf("%s\n\n", input);
+	printf("CAST5 Initial Plaintext:\n");
+	printf("%s\n\n", plaintext);
 	printf("CAST5 Ciphertext (hex representation):\n");
-//	print_output(ciphertext);
+	print_output(ciphertext);
 }
 
-void aes_ctr(unsigned char* input, unsigned char* k, unsigned char* iv) {
+void cast5_decrypt(unsigned char* ciphertext, unsigned char* plaintext, unsigned char* k, unsigned char* iv) {
 	int length;
-	unsigned char ciphertext[512];
+	int plaintext_len = 0;
+	EVP_CIPHER_CTX *ctx;
+
+	ctx = EVP_CIPHER_CTX_new();
+
+	EVP_DecryptInit_ex(ctx, EVP_cast5_cbc(), NULL, k, iv);
+	EVP_DecryptUpdate(ctx, plaintext, &length, ciphertext, strlen((char *)ciphertext));
+	plaintext_len += length;
+	EVP_DecryptFinal_ex(ctx, plaintext + length, &length);
+	plaintext_len += length;
+	EVP_CIPHER_CTX_free(ctx);
+
+	printf("CAST5 Decrypted text:\n");
+	plaintext[plaintext_len] = '\0';
+	printf("%s\n\n", plaintext);
+}
+
+void aes_ctr_encrypt(unsigned char* plaintext, unsigned char* ciphertext, unsigned char* k, unsigned char* iv) {
+	int length;
 	EVP_CIPHER_CTX *ctx;
 
 	ctx = EVP_CIPHER_CTX_new();
 
 	EVP_EncryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, k, iv);
-	EVP_EncryptUpdate(ctx, ciphertext, &length, input, strlen((char *)input));
+	EVP_EncryptUpdate(ctx, ciphertext, &length, plaintext, strlen((char *)plaintext));
 	EVP_EncryptFinal_ex(ctx, ciphertext + length, &length);
 	EVP_CIPHER_CTX_free(ctx);
 
-	printf("AES-CTR Plaintext:\n");
-	printf("%s\n\n", input);
-	printf("AES-CTR Ciphertext (hex representation):\n");
-	//print_output(ciphertext);
+	printf("AES CTR Initial Plaintext:\n");
+	printf("%s\n\n", plaintext);
+	printf("AES CTR Ciphertext (hex representation):\n");
+	print_output(ciphertext);
+}
+
+void aes_ctr_decrypt(unsigned char* ciphertext, unsigned char* plaintext, unsigned char* k, unsigned char* iv) {
+	int length;
+	int plaintext_len = 0;
+	EVP_CIPHER_CTX *ctx;
+
+	ctx = EVP_CIPHER_CTX_new();
+
+	EVP_DecryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, k, iv);
+	EVP_DecryptUpdate(ctx, plaintext, &length, ciphertext, strlen((char *)ciphertext));
+	plaintext_len += length;
+	EVP_DecryptFinal_ex(ctx, plaintext + length, &length);
+	plaintext_len += length;
+	EVP_CIPHER_CTX_free(ctx);
+
+	printf("AES CTR Decrypted text:\n");
+	plaintext[plaintext_len] = '\0';
+	printf("%s\n\n", plaintext);
 }
 
 /***************HASHES******************/
