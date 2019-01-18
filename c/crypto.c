06a35848dd4db2cb9677af1004002b32bd23f59e
Terri Hewitt <hewittt@oregonstate.edu>
2019-01-17 16:46:42 -0800

    Fixed off by 1 error in ciphers

--- c/crypto.c
+++ c/crypto.c
@@ -1,17 +1,83 @@
 #include <stdio.h>	// Only needed for printf
+#include <stdlib.h>	// For rand()
 #include <string.h>
 #include <openssl/sha.h> // For SHA1 and SHA2
 #include <openssl/evp.h>	// For HMAC
 #include <openssl/hmac.h>	
+#include <openssl/des.h>
 
+void triple_des(unsigned char* input, unsigned char* k, unsigned char* iv);
+void aes_ctr(unsigned char* input, unsigned char* k, unsigned char* iv);
 void sha_1(unsigned char*);
 void sha_2(unsigned char*);
 void hmac_sha2(unsigned char*, unsigned char*);
 void hmac_blake2s(unsigned char*, unsigned char*);
 void print_output(unsigned char*);
 
+/***************CIPHERS*****************/
+void triple_des(unsigned char* input, unsigned char* k, unsigned char* iv) {
+	int length;
+	unsigned char ciphertext[512];
+
+	memset(ciphertext, 0x00, sizeof(ciphertext));
+
+	EVP_CIPHER_CTX *ctx;
+
+	ctx = EVP_CIPHER_CTX_new();
+
+	EVP_EncryptInit_ex(ctx, EVP_des_ede3(), NULL, k, iv);
+	EVP_EncryptUpdate(ctx, ciphertext, &length, input, strlen(input) + 1);
+	EVP_EncryptFinal_ex(ctx, ciphertext + length, &length);
+	EVP_CIPHER_CTX_free(ctx);
+
+	printf("3DES Plaintext:\n");
+	printf("%s\n\n", input);
+	printf("3DES Ciphertext (hex representation):\n");
+	print_output(ciphertext);
+}
+
+void cast5(unsigned char* input, unsigned char* k, unsigned char* iv) {
+	int length;
+	unsigned char ciphertext[512];
+	memset(ciphertext, 0x00, sizeof(ciphertext));
+	EVP_CIPHER_CTX *ctx;
+
+	ctx = EVP_CIPHER_CTX_new();
+
+	EVP_EncryptInit_ex(ctx, EVP_cast5_cbc(), NULL, k, iv);
+	EVP_EncryptUpdate(ctx, ciphertext, &length, input, strlen(input) + 1);
+	EVP_EncryptFinal_ex(ctx, ciphertext + length, &length);
+	EVP_CIPHER_CTX_free(ctx);
+
+	printf("CAST5 Plaintext:\n");
+	printf("%s\n\n", input);
+	printf("CAST5 Ciphertext (hex representation):\n");
+	print_output(ciphertext);
+}
+
+void aes_ctr(unsigned char* input, unsigned char* k, unsigned char* iv) {
+	int length;
+	unsigned char ciphertext[512];
+	memset(ciphertext, 0x00, sizeof(ciphertext));
+	EVP_CIPHER_CTX *ctx;
+
+	ctx = EVP_CIPHER_CTX_new();
+
+	EVP_EncryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, k, iv);
+	EVP_EncryptUpdate(ctx, ciphertext, &length, input, strlen(input) + 1);
+	EVP_EncryptFinal_ex(ctx, ciphertext + length, &length);
+	EVP_CIPHER_CTX_free(ctx);
+
+	printf("AES-CTR Plaintext:\n");
+	printf("%s\n\n", input);
+	printf("AES-CTR Ciphertext (hex representation):\n");
+	print_output(ciphertext);
+}
+
+/***************HASHES******************/
 void sha_1(unsigned char* input){
 	unsigned char output[30];
+	memset(output, 0x00, sizeof(output));
 	SHA1(input, strlen(input), output);
 	printf("SHA1: \n");
 	print_output(output);
@@ -19,6 +85,7 @@ void sha_1(unsigned char* input){
 
 void sha_2(unsigned char* input){
 	unsigned char output[30];
+	memset(output, 0x00, sizeof(output));
 	SHA256(input, strlen(input), output);
 	printf("SHA2: \n");
 	print_output(output);
@@ -46,12 +113,22 @@ void print_output(unsigned char* output) {
 }
 
 int main() {
-	unsigned char mssg[] = "This is the message.";
-	unsigned char key[] = "93GWBUGO8T";
+	unsigned char mssg[] = "The quick brown fox jumps over the lazy dog\0";
+//	unsigned char mssg[] = ['0x01', '\0'];
+//	unsigned char key[] = "1\0";
+	unsigned char key256[] = "01234567890123456789012345678901\0";
+//	unsigned char key256[] = ['0x01', '\0'];
+	unsigned char iv128[] = "0123456789012345\0";
+//	unsigned char iv128[] = ['0x02', '\0'];
+
 	sha_1(mssg);
 	sha_2(mssg);
-	hmac_sha2(mssg, key);
-	hmac_blake2s(mssg, key);
+	hmac_sha2(mssg, key256);
+	hmac_blake2s(mssg, key256);
+
+	aes_ctr(mssg, key256, iv128);
+	cast5(mssg, key256, iv128);
+	triple_des(mssg, key256, iv128);
 
 	return 0;
 }
