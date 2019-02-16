eefacae3987365109e991b3989e01d2289941070
Swarles-Barkley <casters@oregonstate.edu>
2019-02-16 10:06:40 -0800

    Fleshed out mqueue (not yet working)

--- dev/mqueue.hpp
+++ dev/mqueue.hpp
@@ -5,14 +5,19 @@
 
 class MessageQueue {
     private:
-        std::vector<Message> messages;
+        //std::vector<Message> messages;
+        std::vector<Message> in_msg;
+	std::vector<Message> out_msg;
         std::vector<std::string> pipes;
         
     public:
-        void push_pipe();
-        void check_pipes();
-        void handle_next_message();
-
+        void push_pipe();	//Sends all out_msgs to 
+        void check_pipes();	//Checks each FD for new incoming msgs
+	void add_pipe();	
+	void remove_pipe();	//nuke everything
+	Message get_next_message();	//return a msg from out_msg and delete after service
+	
+	MessageQueue();
+	~MessageQueue();
 };
-
 #endif
