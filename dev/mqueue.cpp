fcd5ab71119b4e0fdd81837c5e3eeeb794a6bb25
Terri Hewitt <hewittt@oregonstate.edu>
2019-02-21 14:32:22 -0800

    Mostly fixed mqueue (still need Message::get_raw())

--- dev/mqueue.cpp
+++ dev/mqueue.cpp
@@ -1,61 +1,79 @@
+#include "mqueue.hpp"
 #define len_index 7
-mqueue::mqueue(){
-	this->in_msg = new std::vector<Message>;
-	this->out_msg = new std::vector<Message>;
-	this->pipes = new std::vector<std::string>;
+MessageQueue::MessageQueue(){
+	this->in_msg = std::vector<Message>();
+	this->out_msg = std::vector<Message>();
+	this->pipes = std::vector<std::string>();
 }
-mqueue::mqueue(std::vector<std::string> setupPipes){
+MessageQueue::MessageQueue(std::vector<std::string> setupPipes){
 	this->pipes = setupPipes;
-	this->in_msg = new std::vector<Message>;
-	this->out_msg = new std::vector<Message>;
+	this->in_msg = std::vector<Message>();
+	this->out_msg = std::vector<Message>();
 	
 }
-mqueue::~mqueue(){
-	delete this->pipes;
-	delete this->in_msg;
-	delete this->out_msg;
+MessageQueue::~MessageQueue(){
+	this->pipes.clear();
+	this->in_msg.clear();
+	this->out_msg.clear();
 }
-void addPipe(std::string newPipe){
-	pipes.push_back(newPipe);
+void MessageQueue::addPipe(std::string newPipe){
+	this->pipes.push_back(newPipe);
 }
-void removePipe(std::string badPipe){
-	int i;
+void MessageQueue::removePipe(std::string badPipe){
+	unsigned int i;
 	for(i = 0; i<pipes.size(); i++){
 		if(pipes[i].compare(badPipe)!=0){
 			pipes.erase(pipes.begin()+i);
 		}
 	}
 }
-int pushPipes(){//finds *inbound* msgs
-	int i;
-	int start_size = in_msg.size();
+int MessageQueue::pushPipes(){//finds *inbound* msgs
+	unsigned int i;
+	unsigned int start_size = in_msg.size();
+	int fd;
 	for(i = 0; i<start_size; i++){
-		tmp_msg = in_msg[0];
-		const std::string pipe_name = tmp_msg.node_id;
+		Message tmp_msg = in_msg[0];
+		const std::string pipe_name = tmp_msg.get_node_id();
 		tmp_msg.pack();
-		const std::string raw_top = tmp_msp.raw;
-		if((int fd = open(pipe_name, O_APPEND)==-1) return 1;
-		write(fd, raw_top, raw_top, raw_top.length());
-		in_msg.erase(in_msg[0]);
+		//const std::string raw_top = tmp_msg.get_raw();
+		std::string raw_top = "placeholder";
+		if((fd = open((const char*)pipe_name.c_str(), O_APPEND)==-1)) return 1;
+		write(fd, raw_top.c_str(), raw_top.length());
+		in_msg.erase(in_msg.begin());
 	}
-
 	return 0;
 }
-void checkPipes(){//finds *outbound* msgs
-	int i;
+int MessageQueue::checkPipes(){//finds *outbound* msgs
+	unsigned int i;
+	int fd;
 	for(i = 0; i<pipes.size(); i++){
-		const std::string = pipes[i];
-		if((int fd = open(pipe_name, O_RDONLY|O_TRUNCATE)==-1) return 1;
+		const char* pipe_name = pipes[i].c_str();
+		if((fd = open(pipe_name, O_RDONLY|O_TRUNC)==-1)) return 1;
 		
-		const std::string data_len = read(fd, //TODO: Ask Vince about variable length before packet_len w/ 11 nodes in cluster
+		//const std::string data_len = read(fd, //TODO: Ask Vince about variable length before packet_len w/ 11 nodes in cluster
 	}
+	return 0;
 }
-int handle_next_message(){
+Message MessageQueue::get_next_message(){
+	/*
+	if(out_msg.size()<1){
+		return 1;
+	}
+	*/
+	Message top_msg = this->out_msg[0];
+	return top_msg;
+	//this->out_msg.erase(this->out_msg.begin());
+	//TODO things based on top_msg
+}
+
+/*
+int MessageQueue::handle_next_message(){
 	if(out_msg.size()<1){
 		return 1;
 	}
 	top_msg = this->out_msg[0];
-	this->out_msg.erase(this->out_msg[0]);
+	this->out_msg.erase(this->out_msg.begin());
 	//TODO things based on top_msg
 	return 0;
 }
+*/
--- dev/mqueue.hpp
+++ dev/mqueue.hpp
@@ -1,7 +1,28 @@
 #ifndef MQUEUE_HPP
 #define MQUEUE_HPP
 
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
 #include "utility.hpp"
+#include "connection.hpp"
+#include "message.hpp"
+#include "command.hpp"
+#include "node.hpp"
 
 class MessageQueue {
     private:
@@ -11,13 +32,14 @@ class MessageQueue {
         std::vector<std::string> pipes;
         
     public:
-        void push_pipe();	//Sends all out_msgs to 
-        void check_pipes();	//Checks each FD for new incoming msgs
-	void add_pipe();	
-	void remove_pipe();	//nuke everything
+        int pushPipes();	//Sends all out_msgs to 
+        int checkPipes();	//Checks each FD for new incoming msgs
+	void addPipe(std::string);
+	void removePipe(std::string);	//nuke everything
 	Message get_next_message();	//return a msg from out_msg and delete after service
 	
 	MessageQueue();
+	MessageQueue(std::vector<std::string>);
 	~MessageQueue();
 };
 #endif
