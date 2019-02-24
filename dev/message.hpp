825dd781a22b811c7976557c681988df24589e99
vpiscitello <piscitev@oregonstate.edu>
2019-02-24 00:28:01 -0800

    Fixed HMAC Append Bug

--- dev/message.hpp
+++ dev/message.hpp
@@ -119,7 +119,7 @@ class Message {
             if ( this->raw == "" ) {
                 this->pack();
             }
-            return this->raw.append( this->auth_token );
+            return this->raw + this->auth_token;
         }
         /* **************************************************************************
         ** Function: get_response
