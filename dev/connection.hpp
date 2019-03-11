df391ff4ac148df69e310a8bc011131dec1d0ea5
Adam Barton <adamb6102@gmail.com>
2019-03-10 18:37:05 -0700

    Partial commit.

--- dev/connection.hpp
+++ dev/connection.hpp
@@ -537,30 +537,31 @@ class TCP : public Connection {
 	void setup_tcp_socket(std::string port) {
 	    // Creating socket file descriptor 
 	    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
-		perror("socket failed"); 
-		exit(EXIT_FAILURE); 
+		std::cout << "Socket failed\n";
+		return;
 	    } 
 
 	    // Forcefully attaching socket to the port 8080 
 	    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
-		perror("setsockopt"); 
-		exit(EXIT_FAILURE); 
+		std::cout << "Setsockopt failed\n";
+		return;
 	    } 
 	    address.sin_family = AF_INET; 
 	    address.sin_addr.s_addr = INADDR_ANY; 
 	    int PORT = std::stoi(port);
 	    address.sin_port = htons( PORT ); 
 
-	    // Forcefully attaching socket to the port 8080 
 	    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) { 
-		perror("bind failed"); 
-		exit(EXIT_FAILURE); 
+		std::cout << "Bind failed\n";
+		return;
 	    } 
 	    if (listen(server_fd, 3) < 0) { 
-		perror("listen"); 
-		exit(EXIT_FAILURE); 
+		std::cout << "Listen failed\n";
+		return;
 	    } 
 	    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) { 
+		std::cout << "Socket failed\n";
+		return;
 		perror("accept"); 
 		exit(EXIT_FAILURE); 
 	    } 
