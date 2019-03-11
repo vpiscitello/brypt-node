627769fb324a389ec68b45e848a2b8445045807e
Adam Barton <adamb6102@gmail.com>
2019-03-10 19:37:35 -0700

    TCP connection class working partially.

--- dev/connection.hpp
+++ dev/connection.hpp
@@ -4,11 +4,17 @@
 #include <unistd.h>
 #include <vector>
 #include <fstream>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 
 #include "zmq.hpp"
 
 #include "utility.hpp"
 #include "message.hpp"
+
+
 
 class Connection {
     protected:
@@ -436,12 +442,10 @@ class TCP : public Connection {
         std::string peer_IP;
         std::string peer_port;
 
-	int server_fd, new_socket, valread;
+	int server_fd, new_socket;
 	struct sockaddr_in address;
 	int opt = 1;
 	int addrlen = sizeof(address);
-	char buffer[1024] = {0};
-	char * hello = "asdf";
 
     public:
         TCP() {}
@@ -461,73 +465,6 @@ class TCP : public Connection {
                 return;
             }
 
-            //std::cout << "[TCP] Creating normal socket\n";
-            //this->c_pid = fork();
-
-            //switch (this->c_pid) {
-            //    case -1: {
-            //        std::cout << "Error creating child process\n";
-            //        return;
-            //    }
-            //    case 0: {
-            //        sleep(1);
-            //        std::cout << "[TCP] The child id is " << getpid() << "\n";
-            //        this->context = new zmq::context_t(1);
-
-            //        switch (options->operation) {
-            //            case SERVER: {
-            //                //this->socket = new zmq::socket_t(*this->context, ZMQ_STREAM);
-            //                this->socket = new zmq::socket_t(*this->context, ZMQ_REP);
-            //                this->item.socket = *this->socket;
-            //                this->item.events = ZMQ_POLLIN;
-            //                std::cout << "[TCP] Setting up connection on port " << options->port << "... PID: " << getpid() << "\n\n";
-            //                this->instantiate_connection = true;
-            //                this->socket->bind("tcp://*:" + options->port);
-
-            //                handle_messaging();
-            //                CommandType command = INFORMATION_TYPE;
-            //                int phase = 0;
-            //                std::string node_id = "00-00-00-00-00";
-            //                std::string data = "OK";
-            //                unsigned int nonce = 998;
-            //                Message message(node_id, command, phase, data, nonce);
-
-            //                while (1) {
-            //                    std::string req = this->recv();
-            //                    if (req != "") {
-            //                        data = "OK";
-            //                        Message eof_message(node_id, command, phase, data, nonce);
-            //                        Message msg_req(req);
-            //                        if (msg_req.get_data() == "SHUTDOWN") {
-            //                            this->shutdown();
-            //                            this->send(&eof_message);
-            //                            break;
-            //                        } else {
-            //                            this->send(&message);
-            //                        }
-            //                    }
-            //                    sleep(2);
-            //                }
-            //                break;
-            //            }
-            //            case CLIENT: {
-            //                //this->socket = new zmq::socket_t(*this->context, ZMQ_STREAM);
-            //                this->socket = new zmq::socket_t(*this->context, ZMQ_REQ);
-            //                std::cout << "[TCP] Connecting..." << "\n\n";
-            //                this->socket->connect("tcp://" + options->peer_IP + ":" + options->peer_port);
-            //                this->instantiate_connection = false;
-            //                break;
-            //            }
-            //        }
-            //        return;
-            //    }
-            //    default: {
-            //        std::cout << "This parent process id is " << getpid() << " not EXITING\n";
-            //        //exit(0);
-            //        return;
-            //    }
-            //}
-
         }
 
         void whatami() {
@@ -536,104 +473,61 @@ class TCP : public Connection {
 
 	void setup_tcp_socket(std::string port) {
 	    // Creating socket file descriptor 
-	    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
+	    if ((this->server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
 		std::cout << "Socket failed\n";
 		return;
 	    } 
 
-	    // Forcefully attaching socket to the port 8080 
-	    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
+	    if (setsockopt(this->server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &this->opt, sizeof(this->opt))) { 
 		std::cout << "Setsockopt failed\n";
 		return;
 	    } 
-	    address.sin_family = AF_INET; 
-	    address.sin_addr.s_addr = INADDR_ANY; 
+	    this->address.sin_family = AF_INET;
+	    this->address.sin_addr.s_addr = INADDR_ANY;
 	    int PORT = std::stoi(port);
-	    address.sin_port = htons( PORT ); 
+	    this->address.sin_port = htons( PORT );
 
-	    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) { 
+	    if (bind(this->server_fd, (struct sockaddr *)&this->address, sizeof(this->address))<0) {
 		std::cout << "Bind failed\n";
 		return;
 	    } 
-	    if (listen(server_fd, 3) < 0) { 
+	    if (listen(this->server_fd, 30) < 0) {
 		std::cout << "Listen failed\n";
 		return;
 	    } 
-	    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) { 
+	    if ((this->new_socket = accept(this->server_fd, (struct sockaddr *)&this->address, (socklen_t*)&this->addrlen))<0) {
 		std::cout << "Socket failed\n";
-		return;
-		perror("accept"); 
-		exit(EXIT_FAILURE); 
-	    } 
-	    //valread = read( new_socket , buffer, 1024); 
-	    //printf("%s\n",buffer ); 
-	    //send(new_socket , hello , strlen(hello) , 0 ); 
-	    //printf("Hello message sent\n"); 
+	    }
         }
 
-        ////Message * recv(){
-        //std::string recv(){
-        //    //do {
-	//	char buffer[512];
-	//	memset(buffer, '\0', 512);
+        std::string recv(int flag){
+	    char buffer[1024];
+	    memset(buffer, '\0', 1024);
+	    int valread = read(this->new_socket , buffer, 1024);
+	    printf("Received: (%d) %s\n", valread, buffer);
 
-	//	// CHANGE THE 14
-	//	size_t msg_size = zmq_recv(this->socket, buffer, 14, 0);
-	//	std::cout << "Received: " << buffer << "\n";
-	//	memset(buffer, '\0', 512);
-	//	msg_size = zmq_recv(this->socket, buffer, 14, 0);
-	//	std::cout << "Received: " << buffer << "\n";
-	//	memset(buffer, '\0', 512);
-	//	msg_size = zmq_recv(this->socket, buffer, 14, 0);
-	//	std::cout << "Received: " << buffer << "\n";
+	    return buffer;
+    	}
 
-	//	return buffer;
+    	void send(Message * msg) {
+	    std::cout << "[TCP] Sending...\n";
+    	    std::string msg_pack = msg->get_pack();
+	    ssize_t bytes = ::send(new_socket, msg_pack.c_str(), strlen(msg_pack.c_str()), 0);
+    	    std::cout << "[TCP] Sent: (" << (int)bytes << ") " << msg_pack << '\n';
+        }
 
-        //        //if (zmq_poll(&this->item, 1, 100) >= 0) {
-        //        //    if (this->item.revents == 0) {
-        //        //        return "";
-        //        //    }
-        //        //    std::cout << "[TCP] Receiving... PID: " << getpid() << '\n';
-        //        //    zmq::message_t request;
-        //        //    this->socket->recv(&request);
+        void send(const char * message) {
+	    std::cout << "[TCP] Sending...\n";
+	    ssize_t bytes = ::send(new_socket, message, strlen(message), 0);
+    	    std::cout << "[TCP] Sent: (" << (int)bytes << ") " << message << '\n';
+	}
 
-        //        //    std::string req = std::string(static_cast<char *>(request.data()), request.size());
-
-        //        //    std::cout << "[TCP] Received: " << req << "\n";
-
-        //        //    //Message recv_message(req);
-        //        //    //std::cout << "[TCP] WITHIN, id: " << recv_message.get_node_id() << "\n";
-
-        //        //    return req;
-        //        //    //return &recv_message;
-        //        //} else {
-        //        //    std::cout << "Code: " << zmq_errno() << " message: " << zmq_strerror(zmq_errno()) << "\n";
-        //        //    exit(1);
-        //        //}
-        //    //} while ( true );
-    	//}
-
-    	//void send(Message * msg) {
-    	//    std::cout << "[TCP] Sending..." << '\n';
-	//    char * response = "qwer";
-	//    zmq_send(this->socket, id, id_size, ZMQ_SNDMORE);
-	//    zmq_send(this->socket, response, strlen(response), ZMQ_SNDMORE);
-	//    std::cout << "Sent ID followed by result.\n";
-
-    	//    //std::cout << "[TCP] Sending..." << '\n';
-    	//    //std::string msg_pack = msg->get_pack();
-    	//    //zmq::message_t request(msg_pack.size());
-    	//    //memcpy(request.data(), msg_pack.c_str(), msg_pack.size());
-    	//    //this->socket->send(request);
-    	//    //std::cout << "[TCP] Sent: " << msg_pack << '\n';
-        //}
-
-    	//void shutdown() {
+    	void shutdown() {
 	//    // possibly do the send 0 length message thing
     	//    //std::cout << "Shutting down socket and context\n";
     	//    //zmq_close(this->socket);
     	//    //zmq_ctx_destroy(this->context);
-    	//}
+    	}
 
     	//void handle_messaging() {
         //    CommandType command = INFORMATION_TYPE;
@@ -766,6 +660,9 @@ inline Connection* ConnectionFactory(TechnologyType technology) {
         case WEBSOCKET_TYPE:
             return new Websocket;
             break;
+        case TCP_TYPE:
+            return new TCP;
+            break;
         case NONE:
             return NULL;
             break;
@@ -786,6 +683,9 @@ inline Connection* ConnectionFactory(TechnologyType technology, Options *options
             break;
         case WEBSOCKET_TYPE:
             return new Websocket();
+            break;
+        case TCP_TYPE:
+            return new TCP(options);
             break;
         case NONE:
             return NULL;
