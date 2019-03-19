4db83e7aecbc651531e5e399044ebf96f960ddb0
Terri Hewitt <hewittt@oregonstate.edu>
2019-03-18 21:32:50 -0700

    Removed for loop and timer

--- cpp/research/aes-ctr-128/decrypt/main.cpp
+++ cpp/research/aes-ctr-128/decrypt/main.cpp
@@ -1,24 +1,13 @@
 #include "./crypto.h"
 #include <iostream>
-#include <chrono>
-
-typedef std::chrono::high_resolution_clock Clock;
 
 int main() {
 
 	crypto c;
 
-	auto start = Clock::now();
-
-	for (unsigned long long i =0; i < 30000000; i++){
+	while(1){
 		c.aes_ctr_128_decrypt();	
 	}
-
-	auto end = Clock::now();
-
-	std::cout<<"Duration: "<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
-				 	<< " nanoseconds" << std::endl;
-
 
 	return 0;
 }
--- cpp/research/hmac-blake2s/main.cpp
+++ cpp/research/hmac-blake2s/main.cpp
@@ -1,24 +1,13 @@
 #include "./crypto.h"
 #include <iostream>
-#include <chrono>
-
-typedef std::chrono::high_resolution_clock Clock;
 
 int main() {
 
 	crypto c;
-
-	auto start = Clock::now();
-
-	for (unsigned long long i =0; i < 30000000; i++){
+	
+	while(1){
 		c.hmac_blake2s(c.get_plaintext());	
 	}
-
-	auto end = Clock::now();
-
-	std::cout<<"Duration: "<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
-				 	<< " nanoseconds" << std::endl;
-
 
 	return 0;
 }
