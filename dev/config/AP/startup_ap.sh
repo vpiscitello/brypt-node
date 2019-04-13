0433841f44a54b9f0eee8869c985ce83db3c3135
Terri Hewitt <hewittt@oregonstate.edu>
2019-04-12 19:52:16 -0700

    Add network interface file creation

--- dev/config/AP/startup_ap.sh
+++ dev/config/AP/startup_ap.sh
@@ -14,5 +14,18 @@ echo "== Enabling hostapd and dynsmasq"
 echo "== Setup Completed!"
 echo "== Your System Needs to be Rebooted. This will happen automatically in 10 seconds."
 
+cd /home/pi
+touch /home/pi/interfaces
+echo "source-directory /etc/network/interfaces.d" >> /home/pi/interfaces
+echo "auto wlan0" >> /home/pi/interfaces
+echo "iface wlan0 inet static" >> /home/pi/interfaces
+echo "address 192.168.30.1" >> /home/pi/interfaces
+echo "netmask 255.255.255.0\n" >> /home/pi/interfaces
+echo "auto eth0" >> /home/pi/interfaces
+echo "iface eth0 inet dhcp" >> /home/pi/interfaces
+
+sudo rm /etc/network/interfaces
+sudo mv /home/pi/interfaces /etc/network
+
 sleep 10s
 shutdown -h now -r
