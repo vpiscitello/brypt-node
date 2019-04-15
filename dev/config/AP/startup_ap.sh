65f2182b49449c85a9ca74fb4831623a8296ecaf
Terri Hewitt <hewittt@oregonstate.edu>
2019-04-14 17:55:20 -0700

    Copy over Ryan's startup_ap script

--- dev/config/AP/startup_ap.sh
+++ dev/config/AP/startup_ap.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-clear
-
-echo "== Setting Up Brypt Network 802.11 Access Point"
-
-echo "== Storing Old Configuration Files"
-
-echo "== Loading New Configuration Files"
-
-echo "== Enabling hostapd and dynsmasq"
-
-
-echo "== Setup Completed!"
-echo "== Your System Needs to be Rebooted. This will happen automatically in 10 seconds."
+# Replace old rc.local, so reboot doesn't occur again
+sudo cp /home/pi/brypt-node/dev/config/AP/rc.local.base /etc/rc.local
 
 cd /home/pi
 touch /home/pi/interfaces
@@ -27,5 +16,37 @@ echo "iface eth0 inet dhcp" >> /home/pi/interfaces
 sudo rm /etc/network/interfaces
 sudo mv /home/pi/interfaces /etc/network
 
-sleep 10s
-shutdown -h now -r
+if [ -f /root/start_ap ]; then
+				# Copy and configure static IP address
+				cp /home/pi/brypt-node/dev/config/AP/dhcpcd.conf.on /etc/dhcpcd.conf
+				sudo service dhcpcd restart
+
+				# Copy the dnsmasq configuration
+				sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+				sudo cp /home/pi/brypt-node/dev/config/AP/dnsmasq.conf.on /etc/dnsmasq.conf
+
+				# Set the SSID name
+				RAND=`echo $RANDOM | md5sum | cut -b 1-5`
+				sudo echo "ssid=brypt-net-$RAND" >> /home/pi/brypt-node/dev/config/AP/hostapd.conf.on
+				sudo cp /home/pi/brypt-node/dev/config/AP/hostapd.conf.on /etc/hostapd/hostapd.conf
+
+				# Tell hostapd where to find the config file
+				sudo cp /home/pi/brypt-node/dev/config/AP/default-hostapd.on /etc/default/hostapd
+
+				# Ensure hostapd stays live
+				sudo cp /home/pi/brypt-node/dev/config/AP/hostapd.service /etc/systemd/system/hostapd.service
+
+				# Restart the services
+				sudo systemctl start hostapd
+				sudo systemctl start dnsmasq
+
+				# Add routing and masquerade
+				sudo cp /home/pi/brypt-node/dev/config/AP/sysctl.conf.on /etc/sysctl.conf
+				sudo iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
+				sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+				sudo cp /home/pi/brypt-node/dev/config/AP/rc.local.on /etc/rc.local
+
+				# Clean up remaining file
+				sudo rm /root/start_ap
+				sudo reboot
+fi
