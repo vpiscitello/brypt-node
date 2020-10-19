94291501af06161caa48d64dd05c84b9dcb49ef6
Vincenzo Piscitello <v.piscitello@piscitellodesigns.com>
2020-10-18 22:21:23 -0700

    Added First Pass Security Strategy Methods
    - Added method to get the implementation's strategy type. 
    - Added method to set the strategy's key sharing role. 
    - Added methods to synchronize two strategies,  
    - Added methods to encrypt and decrypt messages. 
    - Added methods to sign and verify messages.

--- src/Interfaces/SecurityStrategy.hpp
+++ src/Interfaces/SecurityStrategy.hpp
@@ -4,10 +4,37 @@
 //------------------------------------------------------------------------------------------------
 #pragma once
 //------------------------------------------------------------------------------------------------
+#include "../Components/Security/SecurityTypes.hpp"
+#include "../Components/Security/SecurityDefinitions.hpp"
+//------------------------------------------------------------------------------------------------
+#include <cstdint>
+//------------------------------------------------------------------------------------------------
 
 class ISecurityStrategy {
 public:
     virtual ~ISecurityStrategy() = default;
+
+    virtual Security::Strategy GetStrategyType() const = 0;
+    virtual void SetRole(Security::Role role) = 0;
+
+    virtual std::uint32_t SynchronizationStages() const = 0;
+    virtual Security::SynchronizationStatus SynchronizationStatus() const = 0;
+    virtual Security::SynchronizationResult Synchronize(Security::Buffer const& buffer) = 0;
+
+    virtual Security::OptionalBuffer Encrypt(
+        Security::Buffer const& buffer, std::uint32_t size, std::uint64_t nonce) const = 0;
+    virtual Security::OptionalBuffer Decrypt(
+        Security::Buffer const& buffer, std::uint32_t size, std::uint64_t nonce) const = 0;
+
+    virtual std::uint32_t Sign(Security::Buffer& buffer) const = 0;
+    virtual Security::VerificationStatus Verify(Security::Buffer const& buffer) const = 0;
+
+private: 
+    virtual Security::OptionalBuffer GenerateSignature(
+        std::uint8_t const* pKey,
+        std::uint32_t keySize,
+        std::uint8_t const* pData,
+        std::uint32_t dataSize) const = 0;
 
 };
 
