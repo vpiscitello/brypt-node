258b0a2f593f20ac2b3af79e2ddacda612dbf682
Vincenzo Piscitello <v.piscitello@piscitellodesigns.com>
2021-03-18 22:51:44 -0700

    Added Brypt Library Option and Status Unit Tests

---
+++ src/Tests/UT_BryptLibrary/OptionTest.cpp
@@ -0,0 +1,117 @@
+//----------------------------------------------------------------------------------------------------------------------
+#include <brypt/brypt.hpp>
+//----------------------------------------------------------------------------------------------------------------------
+#include <gtest/gtest.h>
+//----------------------------------------------------------------------------------------------------------------------
+#include <boost/hana.hpp>
+#include <boost/hana/ext/std/tuple.hpp>
+//----------------------------------------------------------------------------------------------------------------------
+#include <any>
+#include <typeindex>
+#include <type_traits>
+#include <vector>
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+namespace {
+namespace local {
+//----------------------------------------------------------------------------------------------------------------------
+
+bool IsExpectedInputType(brypt_option_t option, auto const& input);
+
+template<brypt::suported_primitive_type ValueType>
+bool VerifyValueResult(std::optional<ValueType> const& optValue, auto const& input);
+bool VerifyValueResult(std::optional<std::reference_wrapper<std::string const>> const& optValue, auto const& input);
+
+//----------------------------------------------------------------------------------------------------------------------
+} // local namespace
+//----------------------------------------------------------------------------------------------------------------------
+namespace test {
+//----------------------------------------------------------------------------------------------------------------------
+
+using OptionInputs = boost::hana::tuple<bool, std::int32_t, std::string>;
+using OptionNames = std::vector<brypt_option_t>;
+
+//----------------------------------------------------------------------------------------------------------------------
+} // local namespace
+} // namespace
+//----------------------------------------------------------------------------------------------------------------------
+
+TEST(BryptLibrarySuite, OptionTest)
+{
+    test::OptionInputs const inputs = { true, 35216, "brypt" };
+
+    test::OptionNames const names = {
+        brypt::option::use_bootstraps, 
+        brypt::option::base_path,
+        brypt::option::configuration_filename, 
+        brypt::option::peers_filename,
+    };
+
+    auto VerifyOptionConversion = [this] <brypt::supported_option_type ValueType> (
+        brypt::option const& option, auto const& input, bool isExpectedInput)
+    {
+        EXPECT_EQ(option.is<ValueType>(), isExpectedInput);
+
+        ValueType value;
+        auto const status = option.get(value);
+        EXPECT_EQ(status.succeeded(), isExpectedInput);
+        if (status.succeeded()) { EXPECT_EQ(value, std::any_cast<ValueType const&>(input)); }
+
+        auto const optValue = option.value<ValueType>();
+        EXPECT_EQ(optValue != std::nullopt, isExpectedInput);
+
+        EXPECT_TRUE(local::VerifyValueResult<ValueType>(optValue, input));
+    };
+
+    for (auto const& name : names) {
+        boost::hana::for_each(inputs, [this, &name, &VerifyOptionConversion] (auto const& input) {
+            bool const isExpectedInput = local::IsExpectedInputType(name, input);
+            
+            brypt::status status;
+            brypt::option option(name, input, status);
+            
+            EXPECT_EQ(status.succeeded(), isExpectedInput);
+            EXPECT_EQ(option.has_value(), isExpectedInput);
+
+            VerifyOptionConversion.template operator()<std::decay_t<decltype(input)>>(option, input, isExpectedInput);
+        });
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+bool local::IsExpectedInputType(brypt_option_t option, auto const& input)
+{
+    switch (option) {
+        case brypt::option::use_bootstraps: {
+            return typeid(input) == typeid(bool);
+        }
+        case brypt::option::base_path: 
+        case brypt::option::configuration_filename: 
+        case brypt::option::peers_filename: {
+            return typeid(input) == typeid(std::string);
+        }
+        default: assert(false); return false;
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+template<brypt::suported_primitive_type ValueType> 
+bool local::VerifyValueResult(std::optional<ValueType> const& optValue, auto const& input)
+{
+    if (optValue) { return *optValue == input; }
+    return true;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+bool local::VerifyValueResult(
+    std::optional<std::reference_wrapper<std::string const>> const& optValue, auto const& input)
+{
+    if (optValue) { return optValue.value().get() == input; }
+    return true;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
---
+++ src/Tests/UT_BryptLibrary/StatusTest.cpp
@@ -0,0 +1,67 @@
+//----------------------------------------------------------------------------------------------------------------------
+#include <brypt/brypt.h>
+#include <brypt/brypt.hpp>
+//----------------------------------------------------------------------------------------------------------------------
+#include <gtest/gtest.h>
+//----------------------------------------------------------------------------------------------------------------------
+#include <thread>
+#include <filesystem>
+//----------------------------------------------------------------------------------------------------------------------
+
+TEST(BryptLibrarySuite, StatusDefaultConstructorTest)
+{
+    brypt::status status;
+    EXPECT_TRUE(status.succeeded());
+    EXPECT_FALSE(status.failed());
+    EXPECT_EQ(status, brypt::success_code::accepted);
+    EXPECT_EQ(status.value(), BRYPT_ACCEPTED);
+    EXPECT_STREQ(status.what(), brypt_error_description(BRYPT_ACCEPTED));
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+TEST(BryptLibrarySuite, StatusCAPIConstructorTest)
+{
+    brypt::status status(BRYPT_EOPERTIMEOUT);
+    EXPECT_FALSE(status.succeeded());
+    EXPECT_TRUE(status.failed());
+    EXPECT_EQ(status, brypt::error_code::operation_timeout);
+    EXPECT_EQ(status.value(), BRYPT_EOPERTIMEOUT);
+    EXPECT_STREQ(status.what(), brypt_error_description(BRYPT_EOPERTIMEOUT));
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+TEST(BryptLibrarySuite, StatusSuccessCodeConstructorTest)
+{
+    brypt::status status(brypt::success_code::accepted);
+    EXPECT_TRUE(status.succeeded());
+    EXPECT_FALSE(status.failed());
+    EXPECT_EQ(status, brypt::success_code::accepted);
+    EXPECT_EQ(status.value(), BRYPT_ACCEPTED);
+    EXPECT_STREQ(status.what(), brypt_error_description(BRYPT_ACCEPTED));
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+TEST(BryptLibrarySuite, StatusErrorCodeConstructorTest)
+{
+    brypt::status status(brypt::error_code::unspecified);
+    EXPECT_FALSE(status.succeeded());
+    EXPECT_TRUE(status.failed());
+    EXPECT_EQ(status, brypt::error_code::unspecified);
+    EXPECT_EQ(status.value(), BRYPT_EUNSPECIFIED);
+    EXPECT_STREQ(status.what(), brypt_error_description(BRYPT_EUNSPECIFIED));
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+TEST(BryptLibrarySuite, StatusComparisonTest)
+{
+    brypt::status status(BRYPT_EFILENOTFOUND);
+    EXPECT_EQ(status, brypt::status(brypt::error_code::file_not_found));
+    EXPECT_EQ(status, brypt::error_code::file_not_found);
+    EXPECT_NE(status, brypt::success_code::accepted);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
