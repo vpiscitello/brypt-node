dcb8e0600181de39e25f306bf1f9400b9d543678
Vincenzo Piscitello <v.piscitello@piscitellodesigns.com>
2021-04-01 22:09:01 -0700

    Added Some Build Fixes for MacOS

--- src/cmake/ExternalLibraries.cmake
+++ src/cmake/ExternalLibraries.cmake
@@ -7,6 +7,17 @@ set(DEPENDENCY_DIRECTORY ${CMAKE_SOURCE_DIR}/external)
 set(DEPENDENCY_TEMP_DIRECTORY ${DEPENDENCY_DIRECTORY}/temp)
 set(DEPENDENCY_DOWNLOAD_DIRECTORY ${DEPENDENCY_DIRECTORY}/download)
 
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+    set(SHARED_EXTENSION dll)
+    set(STATIC_EXTENSION lib)
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+    set(SHARED_EXTENSION dylib)
+    set(STATIC_EXTENSION a)
+else()
+    set(SHARED_EXTENSION so)
+    set(STATIC_EXTENSION a)
+endif()
+
 #-----------------------------------------------------------------------------------------------------------------------
 # Boost Dependency
 #-----------------------------------------------------------------------------------------------------------------------
@@ -16,6 +27,10 @@ set(BOOST_URL "https://dl.bintray.com/boostorg/release/${BOOST_VERSION}/source/$
 set(BOOST_HASH "SHA256=aeb26f80e80945e82ee93e5939baebdca47b9dee80a07d3144be1e1a6a66dd6a")
 set(BOOST_DIRECTORY ${DEPENDENCY_DIRECTORY}/boost/${BOOST_VERSION})
 set(BOOST_DOWNLOAD_DIRECTORY ${DEPENDENCY_DOWNLOAD_DIRECTORY}/boost/${BOOST_VERSION})
+
+set(Boost_USE_STATIC_LIBS OFF)
+set(Boost_USE_RELEASE_LIBS ON)
+set(Boost_USE_MULTITHREADED ON)
 
 find_package(Boost ${BOOST_VERSION} COMPONENTS system program_options HINTS ${BOOST_DIRECTORY} QUIET)
 if (NOT Boost_FOUND)
@@ -37,13 +52,38 @@ if (NOT Boost_FOUND)
         message(STATUS "Boost downloaded to ${BOOST_DIRECTORY}")
     endif()
 
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        set(BOOST_TOOLSET gcc)
+        string(REGEX MATCHALL "[0-9]+" GCC_VERSION_COMPONENTS ${CMAKE_CXX_COMPILER_VERSION})
+        list(GET GCC_VERSION_COMPONENTS 0 GCC_MAJOR)
+        set(BOOST_TOOLSET_CONFIGURATION
+            echo "using gcc : ${GCC_MAJOR} : ${CMAKE_CXX_COMPILER} $<SEMICOLON> " >> ${BOOST_DOWNLOAD_DIRECTORY}/tools/build/src/user-config.jam)
+    endif()
+
+    if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+        set(BOOST_FIXUP_DYLIBS
+            bash ${CMAKE_SOURCE_DIR}/cmake/MacOSFixupDylibRpaths.sh ${BOOST_DIRECTORY}/lib)
+    endif()
+    
     set(BOOST_CONFIGURE "./bootstrap.sh")
     set(BOOST_CONFIGURE_PARAMS
-        --prefix=${BOOST_DIRECTORY} --with-python=python3)
+        --prefix=${BOOST_DIRECTORY}
+        --libdir=${BOOST_DIRECTORY}/lib
+        --with-toolset=${BOOST_TOOLSET}
+        --with-python=python3
+        --with-libraries=chrono,program_options,system,timer)
 
     set(BOOST_BUILD "./b2")
     set(BOOST_BUILD_PARAMS
-        --threading=multi --link=shared --variant=release --layout=system --build-type=minimal -j4)
+        --toolset=${BOOST_TOOLSET}
+        --threading=multi
+        --link=shared
+        --variant=release
+        --layout=system
+        --build-type=minimal
+        -sNO_LZMA=1
+        -sNO_ZSTD=1
+        -j4)
 
     ExternalProject_Add(
         boost
@@ -54,23 +94,26 @@ if (NOT Boost_FOUND)
         DOWNLOAD_DIR ""
         DOWNLOAD_COMMAND ""
         UPDATE_COMMAND ""
-        PATCH_COMMAND ""
         CONFIGURE_COMMAND
-            ${BOOST_CONFIGURE} ${BOOST_CONFIGURE_PARAMS}
+            CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
+            ${BOOST_COMPILIER} ${BOOST_CONFIGURE} ${BOOST_CONFIGURE_PARAMS}
+        COMMAND
+            ${BOOST_TOOLSET_CONFIGURATION}
         BUILD_COMMAND
-            ${BOOST_BUILD} ${BOOST_BUILD_PARAMS}
-        INSTALL_COMMAND
             ${BOOST_BUILD} install ${BOOST_BUILD_PARAMS}
+        COMMAND
+            ${BOOST_FIXUP_DYLIBS}
+        INSTALL_COMMAND ""
         INSTALL_DIR ${BOOST_DIRECTORY})
 
     set(Boost_INCLUDE_DIRS ${BOOST_DIRECTORY}/include)
     set(Boost_LIBRARIES
-        ${BOOST_DIRECTORY}/lib/libboost_system.a
-        ${BOOST_DIRECTORY}/lib/libboost_program_options.a)
+        ${BOOST_DIRECTORY}/lib/libboost_system.${SHARED_EXTENSION}
+        ${BOOST_DIRECTORY}/lib/libboost_program_options.${SHARED_EXTENSION})
 endif()
 
 message(DEBUG "Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
-message(DEBUG "Boost_LIBRARY: ${Boost_LIBRARIES}")
+message(DEBUG "Boost_LIBRARIES: ${Boost_LIBRARIES}")
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -107,7 +150,10 @@ if (NOT GTEST_FOUND)
         message(STATUS "Googletest downloaded to ${GOOGLETEST_DIRECTORY}")
     endif()
 
-    set(GOOGLETEST_CONFIGURE_PARAMS -DCMAKE_INSTALL_PREFIX=${GOOGLETEST_DIRECTORY})
+    set(GOOGLETEST_CONFIGURE_PARAMS
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} 
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_INSTALL_PREFIX=${GOOGLETEST_DIRECTORY})
     
     ExternalProject_Add(
         googletest
@@ -118,12 +164,11 @@ if (NOT GTEST_FOUND)
         DOWNLOAD_DIR ""
         DOWNLOAD_COMMAND ""
         UPDATE_COMMAND ""
-        PATCH_COMMAND ""
         CMAKE_ARGS ${GOOGLETEST_CONFIGURE_PARAMS}
         INSTALL_DIR ${GOOGLETEST_DIRECTORY})
 
     set(GTEST_INCLUDE_DIRS ${GOOGLETEST_DIRECTORY}/include)
-    set(GTEST_LIBRARIES ${GOOGLETEST_DIRECTORY}/lib/libgtest.a)
+    set(GTEST_LIBRARIES ${GOOGLETEST_DIRECTORY}/lib/libgtest.${STATIC_EXTENSION})
 endif()
 
 message(DEBUG "GTEST_INCLUDE_DIRS: ${GTEST_INCLUDE_DIRS}")
@@ -207,7 +252,20 @@ if (NOT OPENSSL_FOUND)
         no-psk no-srp no-dso no-dsa no-rc2 no-des)
     set(OPENSSL_CONFIGURE_PARAMS
         --prefix=${OPENSSL_DIRECTORY} --openssldir=${OPENSSL_DIRECTORY} --libdir=lib)
-    
+
+    set(OPENSSL_BUILD_TARGET build_generated)
+    set(OPENSSL_INSTALL_TARGET install_sw)
+    if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+        set(OPENSSL_KERNEL_BITS KERNEL_BITS=64)
+        set(OPENSSL_MAKE_TARGET build_libs)
+        set(OPENSSL_INSTALL_TARGET install_dev)
+    endif()
+
+    if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+        set(OPENSSL_FIXUP_DYLIBS
+            bash ${CMAKE_SOURCE_DIR}/cmake/MacOSFixupDylibRpaths.sh ${OPENSSL_DIRECTORY}/lib)
+    endif()
+
     find_program(OPENSSL_BUILD make REQUIRED)
 
     ExternalProject_Add(
@@ -219,17 +277,20 @@ if (NOT OPENSSL_FOUND)
         DOWNLOAD_DIR ""
         DOWNLOAD_COMMAND ""
         UPDATE_COMMAND ""
-        PATCH_COMMAND ""
         CONFIGURE_COMMAND
+            CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} ${OPENSSL_KERNEL_BITS}
+        COMMAND
             ${OPENSSL_CONFIGURE} ${OPENSSL_CONFIGURE_MODULES} ${OPENSSL_CONFIGURE_PARAMS}
         BUILD_COMMAND
-            ${OPENSSL_BUILD} build_generated
+            ${OPENSSL_BUILD} ${OPENSSL_MAKE_TARGET}
         INSTALL_COMMAND
-            ${OPENSSL_BUILD} install_sw
+            ${OPENSSL_BUILD} ${OPENSSL_INSTALL_TARGET}
+        COMMAND
+            ${OPENSSL_FIXUP_DYLIBS}
         INSTALL_DIR ${OPENSSL_DIRECTORY})
 
     set(OPENSSL_INCLUDE_DIR ${OPENSSL_DIRECTORY}/include)
-    set(OPENSSL_CRYPTO_LIBRARY ${OPENSSL_DIRECTORY}/lib/libcrypto.so)    
+    set(OPENSSL_CRYPTO_LIBRARY ${OPENSSL_DIRECTORY}/lib/libcrypto.${SHARED_EXTENSION})    
 endif()
 
 message(DEBUG "OPENSSL_INCLUDE_DIR: ${OPENSSL_INCLUDE_DIR}")
@@ -246,7 +307,7 @@ set(OQS_HASH "SHA256=05836cd2b5c70197b3b6eed68b97d0ccb2c445061d5c19c15aef7c95984
 set(OQS_DIRECTORY ${DEPENDENCY_DIRECTORY}/liboqs/${OQS_VERSION})
 set(OQS_DOWNLOAD_DIRECTORY ${DEPENDENCY_DOWNLOAD_DIRECTORY}/liboqs/${OQS_VERSION})
 
-if (NOT EXISTS ${OQS_DIRECTORY})
+if (NOT EXISTS ${OQS_DOWNLOAD_DIRECTORY})
     set(OQS_BUILD_REQUIRED true)
 
     message(STATUS "Open Quantum Safe (liboqs) was not found. Downloading now...")
@@ -266,10 +327,13 @@ if (NOT EXISTS ${OQS_DIRECTORY})
     message(STATUS "Open Quantum Safe (liboqs) downloaded to ${DEPENDENCY_DOWNLOAD_DIRECTORY}")
 endif()
 
-if (NOT EXISTS ${OQS_DIRECTORY}/lib/liboqs.so)
+if (NOT EXISTS ${OQS_DIRECTORY}/lib/liboqs.${SHARED_EXTENSION})
     set(OQS_CONFIGURE_GENERATOR "Ninja")
     set(OQS_CONFIGURE_PARAMS
-        -DBUILD_SHARED_LIBS=ON -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR} -DCMAKE_INSTALL_PREFIX=${OQS_DIRECTORY})
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        -DBUILD_SHARED_LIBS=ON
+        -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}
+        -DCMAKE_INSTALL_PREFIX=${OQS_DIRECTORY})
     find_program(OQS_BUILD ninja REQUIRED)
 
     ExternalProject_Add(
@@ -281,7 +345,6 @@ if (NOT EXISTS ${OQS_DIRECTORY}/lib/liboqs.so)
         DOWNLOAD_DIR ""
         DOWNLOAD_COMMAND ""
         UPDATE_COMMAND ""
-        PATCH_COMMAND ""
         CMAKE_GENERATOR ${OQS_CONFIGURE_GENERATOR}
         CMAKE_ARGS ${OQS_CONFIGURE_PARAMS}
         BUILD_COMMAND ${OQS_BUILD}
@@ -294,7 +357,7 @@ if (NOT EXISTS ${OQS_DIRECTORY}/lib/liboqs.so)
 endif()
 
 set(OQS_INCLUDE_DIRS ${OQS_DIRECTORY}/include)
-set(OQS_LIBRARIES ${OQS_DIRECTORY}/lib/liboqs.so)    
+set(OQS_LIBRARIES ${OQS_DIRECTORY}/lib/liboqs.${SHARED_EXTENSION})    
 
 message(DEBUG "OQS_INCLUDE_DIRS: ${OQS_INCLUDE_DIRS}")
 message(DEBUG "OQS_LIBRARIES: ${OQS_LIBRARIES}")
@@ -363,8 +426,11 @@ if (NOT EXISTS ${SPDLOG_DOWNLOAD_DIRECTORY})
     message(STATUS "spdlog downloaded to ${SPDLOG_DIRECTORY}")
 endif()
     
-if (NOT EXISTS ${SPDLOG_DIRECTORY}/lib/libspdlog.a)
-    set(SPDLOG_CONFIGURE_PARAMS -DCMAKE_INSTALL_PREFIX=${SPDLOG_DIRECTORY})
+if (NOT EXISTS ${SPDLOG_DIRECTORY}/lib/libspdlog.${SHARED_EXTENSION})
+    set(SPDLOG_CONFIGURE_PARAMS
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_INSTALL_PREFIX=${SPDLOG_DIRECTORY}
+        -DSPDLOG_BUILD_SHARED=ON)
     
     ExternalProject_Add(
         spdlog
@@ -375,13 +441,12 @@ if (NOT EXISTS ${SPDLOG_DIRECTORY}/lib/libspdlog.a)
         DOWNLOAD_DIR ""
         DOWNLOAD_COMMAND ""
         UPDATE_COMMAND ""
-        PATCH_COMMAND ""
         CMAKE_ARGS ${SPDLOG_CONFIGURE_PARAMS}
         INSTALL_DIR ${SPDLOG_DIRECTORY})
 endif()
 
 set(spdlog_INCLUDE_DIRS ${SPDLOG_DIRECTORY}/include)
-set(spdlog_LIBRARIES ${SPDLOG_DIRECTORY}/lib/libspdlog.a)
+set(spdlog_LIBRARIES ${SPDLOG_DIRECTORY}/lib/libspdlog.${SHARED_EXTENSION})
 
 message(DEBUG "spdlog_INCLUDE_DIRS: ${spdlog_INCLUDE_DIRS}")
 message(DEBUG "spdlog_LIBRARIES: ${spdlog_LIBRARIES}")
