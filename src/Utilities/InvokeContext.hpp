0b36f26231b1dbcb3a67717a28116d256dc988cc
Vincenzo Piscitello <v.piscitello@piscitellodesigns.com>
2021-06-19 14:34:21 -0700

    Added Macro to Define Testing Only Functions

--- src/Utilities/InvokeContext.hpp
+++ src/Utilities/InvokeContext.hpp
@@ -12,4 +12,8 @@ enum class InvokeContext : std::uint32_t { Production, Test };
 template<InvokeContext ContextType>
 concept TestingContext = requires { ContextType == InvokeContext::Test; };
 
+#define UT_SupportMethod(declaration) \
+template <InvokeContext ContextType = InvokeContext::Production> requires TestingContext<ContextType> \
+declaration
+
 //----------------------------------------------------------------------------------------------------------------------
