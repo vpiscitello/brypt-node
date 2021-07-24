49854d7ce3c45f2739b1721361392d5839b4705d
Vincenzo Piscitello <v.piscitello@piscitellodesigns.com>
2021-07-23 22:23:26 -0700

    Updated the CLI Application to Handle Shutdown Signals
    - The CLI application can now handle SIGINT and SIGTERM (e.g. "Control-C") to request a shutdown of the node. This takes advantage of the new execution token. 
    - Cleaned up main.cpp to separate startup option parsing and resource initialization from starting execution of the core. The Startup::Options object does not need to be kept alive after the options are stored in the configuration parser.

--- src/BryptNode/main.cpp
+++ src/BryptNode/main.cpp
@@ -3,6 +3,7 @@
 // Description:
 //----------------------------------------------------------------------------------------------------------------------
 #include "BryptNode.hpp"
+#include "ExecutionToken.hpp"
 #include "RuntimeContext.hpp"
 #include "StartupOptions.hpp"
 #include "BryptIdentifier/BryptIdentifier.hpp"
@@ -11,72 +12,136 @@
 #include "Components/Configuration/BootstrapService.hpp"
 #include "Components/Event/Publisher.hpp"
 #include "Components/MessageControl/AuthorizedProcessor.hpp"
-#include "Components/MessageControl/DiscoveryProtocol.hpp"
 #include "Components/Network/EndpointTypes.hpp"
 #include "Components/Network/Manager.hpp"
 #include "Components/Peer/Manager.hpp"
 #include "Utilities/Assertions.hpp"
-#include "Utilities/ExecutionResult.hpp"
+#include "Utilities/ExecutionStatus.hpp"
 #include "Utilities/LogUtils.hpp"
 #include "Utilities/Version.hpp"
 //----------------------------------------------------------------------------------------------------------------------
+#include <boost/core/quick_exit.hpp>
+//----------------------------------------------------------------------------------------------------------------------
 #include <cassert>
+#include <csignal>
 #include <cstdint>
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+namespace {
+namespace Signal {
+//----------------------------------------------------------------------------------------------------------------------
+
+Node::ExecutionToken ExecutionToken;
+
+extern "C" void OnShutdownRequested(std::int32_t signal);
+
+//----------------------------------------------------------------------------------------------------------------------
+} // Signal namespace
+} // namespace
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+namespace Startup {
+//----------------------------------------------------------------------------------------------------------------------
+
+using Resources = std::tuple<ParseCode, std::unique_ptr<Configuration::Parser>, std::shared_ptr<BootstrapService>>;
+
+Resources InitializeResources(std::int32_t argc, char** argv);
+
+//----------------------------------------------------------------------------------------------------------------------
+} // Startup namespace
+//----------------------------------------------------------------------------------------------------------------------
+
+extern "C" void Signal::OnShutdownRequested(std::int32_t signal)
+{
+    // Note: std::quick_exit() is for some reason still unimplemented on MacOS, boost::quick_exit will alias to the 
+    // next best alternative (e.g. __Exit()). We could use abort(), but we would loose the conext of the caught signal.
+    // If we haven't started the node yet, there is no additional cleanup required (std::exit() is not signal safe).
+    if (!ExecutionToken.IsExecutionActive()) { boost::quick_exit(signal); }
+    // Use the token to notify the node that a shutdown has been requested. In the context of the console application,
+    // SIGINT and SIGTERM are considered are expected shutdown signals (i.e. not errors).
+    [[maybe_unused]] bool const result = ExecutionToken.RequestStop();
+}
+
 //----------------------------------------------------------------------------------------------------------------------
 
 std::int32_t main(std::int32_t argc, char** argv)
 {
-    Startup::Options options;
-    if (auto const status = options.Parse(argc, argv); status != Startup::ParseCode::Success) {
-        if (status == Startup::ParseCode::ExitRequested) { return 0; }
-        else { std::cout << "Unable to parse startup options!" << std::endl; return 1; }
-    }
-    
     assert(Assertions::Threading::IsCoreThread()); // Note: We must ensure the main thread id is set.
-    LogUtils::InitializeLoggers(options.GetVerbosityLevel()); // Note: We must ensure the loggers are initialized.
-    auto const spLogger = spdlog::get(LogUtils::Name::Core.data());  
 
-    auto const upParser = std::make_unique<Configuration::Parser>(options.GetConfigPath(), options.IsInteractive());
-    if (auto const status = upParser->FetchSettings(); status != Configuration::StatusCode::Success) {
-        spLogger->critical("An unexpected error occured while parsing the configuration file!");
-        return 1;
+    // Register listeners such that we can properly handle shutdown requests via process signals. 
+    if (SIG_ERR == std::signal(SIGINT, Signal::OnShutdownRequested)) { return 1; }
+    if (SIG_ERR == std::signal(SIGTERM, Signal::OnShutdownRequested)) { return 1; }
+
+    auto const [code, upParser, spBootstrapService] = Startup::InitializeResources(argc, argv);
+    switch (code) {
+        case Startup::ParseCode::Success: assert(upParser && spBootstrapService); break;
+        case Startup::ParseCode::ExitRequested: return 0;
+        case Startup::ParseCode::Malformed: return 1;
     }
 
-    auto const spBootstrapService = std::make_shared<BootstrapService>(
-        options.GetBootstrapPath(), upParser->GetEndpointOptions());
-    if (!spBootstrapService->FetchBootstraps()) {
-        spLogger->critical("An error occured parsing bootstraps!");
-        return 1;
+    auto const logger = spdlog::get(LogUtils::Name::Core.data());  
+    logger->info("Welcome to the Brypt Network!");
+    logger->info("Brypt Identifier: {}", upParser->GetNodeIdentifier());
+
+    Node::Core core(Signal::ExecutionToken, upParser, spBootstrapService);
+
+    auto const status = core.Startup(); // Start the core, the runtime will block will until execution completes.
+    switch (status) {
+        case ExecutionStatus::RequestedShutdown: break;
+        // Log out an error and return a non-success code if the runtime is shutdown due to an unexpected error. 
+        case ExecutionStatus::UnexpectedShutdown: {
+            logger->critical("An unexpected error caused the node to shutdown!");
+            return 1;
+        }
+         // Currently, only the two explicit status cases are expected for a foreground process. 
+        default: assert(false); return 1;
     }
-
-    auto const spPublisher = std::make_shared<Event::Publisher>();
-    auto const spProtocol = std::make_shared<DiscoveryProtocol>(upParser->GetEndpointOptions());
-    auto const spProcessor = std::make_shared<AuthorizedProcessor>(upParser->GetNodeIdentifier());
-    auto const spPeerManager = std::make_shared<Peer::Manager>(
-        upParser->GetNodeIdentifier(), upParser->GetSecurityStrategy(), spProtocol, spProcessor);
-
-    spBootstrapService->SetMediator(spPeerManager.get());
-
-    IBootstrapCache const* const pBootstraps = (options.UseBootstraps()) ? spBootstrapService.get() : nullptr;
-    auto const spNetworkManager = std::make_shared<Network::Manager>(
-        upParser->GetEndpointOptions(), spPublisher, spPeerManager.get(), pBootstraps, RuntimeContext::Foreground);
-
-    BryptNode alpha(upParser, spPublisher, spNetworkManager, spPeerManager, spProcessor, spBootstrapService);
-    assert(alpha.IsInitialized());
-
-    spLogger->info("Welcome to the Brypt Network!");
-    spLogger->info("Brypt Identifier: {}", upParser->GetNodeIdentifier());
-
-    ExecutionResult const result = alpha.Startup();
-    if (result == ExecutionResult::UnexpectedShutdown) {
-        spLogger->critical("An unexpected error caused the node to shutdown!");
-        return 1;
-    }
-    // Currently, RequestedShutdown and UnexpectedShutdown are the only values that should be returned in the 
-    // foreground context. If we get InitializationFailed, AlreadyStarted, or ThreadSpawned something went wrong 
-    assert(result == ExecutionResult::RequestedShutdown);
 
     return 0;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+Startup::Resources Startup::InitializeResources(std::int32_t argc, char** argv)
+{
+    Options options; 
+    switch (options.Parse(argc, argv)) {
+        // On success, we can continue directly to initializing the runtime resources. 
+        case ParseCode::Success: break;
+        // Early return when we don't need initialize the resources (e.g. when "--help" is used).
+        case ParseCode::ExitRequested: return { ParseCode::ExitRequested, nullptr, nullptr }; 
+        // Log out an errur and early return when the provided flags were malformed. 
+        default: {
+            std::cout << "Unable to parse startup options!" << std::endl;
+            return { ParseCode::Malformed, nullptr, nullptr }; 
+        }
+    }
+    
+    // Initialize the logging resources for the application. 
+    LogUtils::InitializeLoggers(options.GetVerbosityLevel()); 
+    auto const logger = spdlog::get(LogUtils::Name::Core.data()); // From here on we should use the logger for errors. 
+
+    // Create a configuration parser to read the configuration file at the provided location. If we fail to read the
+    // file log an error and return early. 
+    auto upParser = std::make_unique<Configuration::Parser>(options.GetConfigPath(), options);
+    if (auto const status = upParser->FetchOptions(); status != Configuration::StatusCode::Success) {
+        logger->critical("An unexpected error occured while parsing the configuration file!");
+        return { ParseCode::Malformed, nullptr, nullptr };
+    }
+
+    // Create the bootstrap service and read the initial bootstraps from the file. If we fail to fetch the bootstraps,
+    // log an error and return early. 
+    auto spBootstrapService = std::make_shared<BootstrapService>(
+        options.GetBootstrapPath(), upParser->GetEndpointOptions(), upParser->UseFilepathDeduction());
+    if (!spBootstrapService->FetchBootstraps()) {
+        logger->critical("An error occured parsing bootstraps!");
+        return { ParseCode::Malformed, nullptr, nullptr };
+    }
+
+    // Indicate that the startup resources have been successfully initialized and provide them to the caller. 
+    return { ParseCode::Success, std::move(upParser), std::move(spBootstrapService) };
 }
 
 //----------------------------------------------------------------------------------------------------------------------
