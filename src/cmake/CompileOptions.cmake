aacf3fed121ebe6cf16078fd444dfbe1fa252b72
Vincenzo Piscitello <v.piscitello@piscitellodesigns.com>
2024-09-30 22:12:18 -0700

    Updating Base CMake Files for Library and Security Changes
    - Added compile options to compile for library targets.
    - Added compile options to configure the stack size for quantum algorithm support.
    - Updating pathing for recent file moves.

--- src/cmake/CompileOptions.cmake
+++ src/cmake/CompileOptions.cmake
@@ -25,15 +25,29 @@ function(set_project_options project_name)
             -Wno-missing-field-initializers
             -Wno-missing-braces
             -Wformat=2)
-        set(PROJECT_FEATURES -fcoroutines -fvisibility=hidden -fvisibility-inlines-hidden)
+        
+        if(CMAKE_BUILD_TYPE STREQUAL "Release")
+            set(PROJECT_FEATURES 
+                -fdata-sections
+                -ffunction-sections
+                -Wl,--gc-sections,--stack,8388608)
+        endif()
+
+        set(PROJECT_FEATURES 
+            -fcoroutines
+            -fvisibility=hidden
+            -fvisibility-inlines-hidden
+            ${EXTRA_PROJECT_FEATURES})
+
         set(PROJECT_LINKS -pthread)
 
         set(PROJECT_DEFINITIONS 
             -DSPDLOG_DISABLE_DEFAULT_LOGGER=1
             -DSPDLOG_NO_THREAD_ID=1)
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-        set(PROJECT_WARNINGS /W4)
-        set(PROJECT_FEATURES /Zc:preprocessor /Zc:__cplusplus /await:strict)
+        set(PROJECT_FEATURES /EHsc /c /Zc:preprocessor /Zc:__cplusplus /await:strict /sdl /w34996)
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /STACK:8388608")
+        set(PROJECT_LINKS "/STACK:8388608")
 
         macro(get_version_tag version)
             string(REGEX MATCH "^([0-9]+)\\.([0-9]+)" ${version} ${CMAKE_SYSTEM_VERSION})
@@ -43,12 +57,15 @@ function(set_project_options project_name)
         get_version_tag(windows_version_tag)
 
         set(PROJECT_DEFINITIONS 
+            WIN_EXPORT
             -D_WIN32_WINNT=${windows_version_tag}
             -DNOMINMAX
             -DBOOST_ALL_NO_LIB
             -DSPDLOG_DISABLE_DEFAULT_LOGGER
             -DSPDLOG_NO_THREAD_ID
             -DSPDLOG_WCHAR_TO_UTF8_SUPPORT)
+
+        set_target_properties(${project_name} PROPERTIES CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
     endif()
 
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
